1593b349a72a0ea72ceb96a93d4f035acbcdea6e
Louis Fletcher <Louisco1999@gmail.com>
2021-10-18 17:07:47 +0100

    initial commit

---
+++ precourse_recap.py
@@ -0,0 +1,14 @@
+
+user_name = input("please enter your name")
+
+print("good luck " + user_name +" p.s the colour is case sensitive")
+
+favourite_colour = "blue"
+
+guess = input("guess the colour")
+
+if guess == "blue" :
+
+    print("Well done!!")
+else :
+    print("thats wrong guess again please")
